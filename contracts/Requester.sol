a9dbdf26fe4a9dbdfafdfd6956e950cc877ff7e4
vanshwassan <007blackhacker@gmail.com>
2022-10-22 21:57:06 +0530

    Initial Commit

---
+++ contracts/Requester.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.8.9;
+
+import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+
+// A Requester that will return the requested data by calling the specified airnode.
+// Make sure you specify the right _rrpAddress for your chain.
+
+contract Requester is RrpRequesterV0 {
+    mapping(bytes32 => bool) public incomingFulfillments;
+    mapping(bytes32 => int256) public fulfilledData;
+
+    constructor(address _rrpAddress) RrpRequesterV0(_rrpAddress) {}
+
+    /**
+     * The main makeRequest function that will trigger the Airnode request
+     * airnode: Airnode address
+     * endpointId: The endpoint ID for the specific endpoint
+     * sponsor: The requester contract itself (in this case)
+     * sponsorWallet: The wallet that will make the actual request (needs to be funded)
+     * parameters: encoded API parameters
+     */
+    function makeRequest(
+        address airnode,
+        bytes32 endpointId,
+        address sponsor,
+        address sponsorWallet,
+        bytes calldata parameters
+        
+    ) external {
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnode,
+            endpointId,
+            sponsor,
+            sponsorWallet,
+            address(this),
+            this.fulfill.selector,
+            parameters
+        );
+        incomingFulfillments[requestId] = true;
+    }
+
+    // The callback function with the requested data
+    function fulfill(bytes32 requestId, bytes calldata data)
+        external
+        onlyAirnodeRrp
+    {
+        require(incomingFulfillments[requestId], "No such request made");
+        delete incomingFulfillments[requestId];
+        int256 decodedData = abi.decode(data, (int256));
+        fulfilledData[requestId] = decodedData;
+    }
+}
